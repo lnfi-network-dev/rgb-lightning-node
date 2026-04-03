0161b92ac07f26e4f3061c03a4e58c0ea2389343
ka <ka@ln.exchange>
2026-04-03 08:51:18 +0800

    Refactor routes and clean up unused structs and asset handling

--- src/routes.rs
+++ src/routes.rs
@@ -248,25 +248,6 @@ impl From<RgbLibAssetIFA> for AssetIFA {
 }
 
 #[derive(Deserialize, Serialize)]
-pub(crate) struct AssetMetadataRequest {
-    pub(crate) asset_id: String,
-}
-
-#[derive(Deserialize, Serialize)]
-pub(crate) struct AssetMetadataResponse {
-    pub(crate) asset_schema: AssetSchema,
-    pub(crate) initial_supply: u64,
-    pub(crate) max_supply: u64,
-    pub(crate) known_circulating_supply: u64,
-    pub(crate) timestamp: i64,
-    pub(crate) name: String,
-    pub(crate) precision: u8,
-    pub(crate) ticker: Option<String>,
-    pub(crate) details: Option<String>,
-    pub(crate) token: Option<Token>,
-}
-
-#[derive(Deserialize, Serialize)]
 pub(crate) struct AssetNIA {
     pub(crate) asset_id: String,
     pub(crate) ticker: String,
@@ -1979,84 +1960,6 @@ pub(crate) async fn get_channel_id(
     };
 
     Ok(Json(GetChannelIdResponse { channel_id }))
-}
-
-pub(crate) async fn get_payment(
-    State(state): State<Arc<AppState>>,
-    WithRejection(Json(payload), _): WithRejection<Json<GetPaymentRequest>, APIError>,
-) -> Result<Json<GetPaymentResponse>, APIError> {
-    let guard = state.check_unlocked().await?;
-    let unlocked_state = guard.as_ref().unwrap();
-
-    let payment_hash_vec = hex_str_to_vec(&payload.payment_hash);
-    if payment_hash_vec.is_none() || payment_hash_vec.as_ref().unwrap().len() != 32 {
-        return Err(APIError::InvalidPaymentHash(payload.payment_hash));
-    }
-    let requested_ph = PaymentHash(payment_hash_vec.unwrap().try_into().unwrap());
-
-    let inbound_payments = unlocked_state.list_updated_inbound_payments();
-    let outbound_payments = unlocked_state.outbound_payments();
-
-    for (payment_hash, payment_info) in &inbound_payments {
-        if payment_hash == &requested_ph {
-            let rgb_payment_info_path_inbound =
-                get_rgb_payment_info_path(payment_hash, &state.static_state.ldk_data_dir, true);
-
-            let (asset_amount, asset_id) = if rgb_payment_info_path_inbound.exists() {
-                let info = parse_rgb_payment_info(&rgb_payment_info_path_inbound);
-                (Some(info.amount), Some(info.contract_id.to_string()))
-            } else {
-                (None, None)
-            };
-
-            return Ok(Json(GetPaymentResponse {
-                payment: Payment {
-                    amt_msat: payment_info.amt_msat,
-                    asset_amount,
-                    asset_id,
-                    payment_hash: hex_str(&payment_hash.0),
-                    inbound: true,
-                    status: payment_info.status,
-                    created_at: payment_info.created_at,
-                    updated_at: payment_info.updated_at,
-                    payee_pubkey: payment_info.payee_pubkey.to_string(),
-                    preimage: payment_info.preimage.map(|p| hex_str(&p.0)),
-                },
-            }));
-        }
-    }
-
-    for (payment_id, payment_info) in &outbound_payments {
-        let payment_hash = &PaymentHash(payment_id.0);
-        if payment_hash == &requested_ph {
-            let rgb_payment_info_path_outbound =
-                get_rgb_payment_info_path(payment_hash, &state.static_state.ldk_data_dir, false);
-
-            let (asset_amount, asset_id) = if rgb_payment_info_path_outbound.exists() {
-                let info = parse_rgb_payment_info(&rgb_payment_info_path_outbound);
-                (Some(info.amount), Some(info.contract_id.to_string()))
-            } else {
-                (None, None)
-            };
-
-            return Ok(Json(GetPaymentResponse {
-                payment: Payment {
-                    amt_msat: payment_info.amt_msat,
-                    asset_amount,
-                    asset_id,
-                    payment_hash: hex_str(&payment_hash.0),
-                    inbound: false,
-                    status: payment_info.status,
-                    created_at: payment_info.created_at,
-                    updated_at: payment_info.updated_at,
-                    payee_pubkey: payment_info.payee_pubkey.to_string(),
-                    preimage: payment_info.preimage.map(|p| hex_str(&p.0)),
-                },
-            }));
-        }
-    }
-
-    Err(APIError::PaymentNotFound(payload.payment_hash))
 }
 
 pub(crate) async fn get_swap(
